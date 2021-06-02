d49edc6c31c6717935a0d4c185893e5c394bdf76
mobinaa81 <85250980+mobinaa81@users.noreply.github.com>
2021-06-03 03:40:32 +0430

    Create Homework_BMI.py

---
+++ Homework_BMI.py
@@ -0,0 +1,14 @@
+#mohasebe BMI
+print("salam be mohasebegar BMI khosh amadid ^_^")
+qad = float(input("lotfan ghad khod ra vared konid(m)\n"))
+vazn = float(input("lotfan vazn khod ra vared konid(kg)\n"))
+BMI = vazn/(qad**2)
+
+if BMI < 18.5 :
+    print("shoma kambood vazn darid :(")
+if 18.5 <= BMI <= 24.9 :
+    print("shoma vazn monaseb darid :D")
+if 24.9 < BMI <= 29.9 :
+    print("shoma kami ezafe vann darid :(")
+if BMI > 29.9 :
+    print("shoma ezafe vazn  shadid darid :O")
