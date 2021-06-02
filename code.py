4f513a52d2cfccd37ebf67b93c664365434fda57
mobinaa81 <85250980+mobinaa81@users.noreply.github.com>
2021-06-02 22:39:46 +0430

    Create code.py

---
+++ code.py
@@ -0,0 +1,11 @@
+print("yek adad vared kon")
+first_number = input()
+first_number_float = float(first_number)
+
+print("yek adad digar ham vared kon")
+second_number = input()
+second_number_float = float(second_number)
+
+result = first_number_float + second_number_float
+
+print(result)
