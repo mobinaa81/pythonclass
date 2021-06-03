d6faf98c7026ddbc9c88e2e68e4c35d62b19ec5f
mobinaa81 <85250980+mobinaa81@users.noreply.github.com>
2021-06-03 14:28:53 +0430

    Create bmi_def.py

---
+++ bmi_def.py
@@ -0,0 +1,6 @@
+def BMI ():
+    qad = float(input("lotfan ghad khod ra vared konid(m)\n"))
+    vazn = float(input("lotfan vazn khod ra vared konid(kg)\n"))
+    BMI = vazn/(qad**2)
+    print("BMI shoma =" , BMI)
+BMI()
