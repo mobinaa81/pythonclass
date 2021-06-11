0a3ee6a1366fa46aa4c85f0bab40edd190f0f237
mobinaa81 <85250980+mobinaa81@users.noreply.github.com>
2021-06-11 20:25:53 +0430

    Create home4_1.py

---
+++ home4_1.py
@@ -0,0 +1,13 @@
+adad = 0
+while adad <= 1000 :
+    if adad % 2  == 0 :
+        print(f"{adad}  :  in adad bahale!")
+    elif adad % 4  == 0 :
+        print(f"{adad}  :  in adad bahale!")
+    elif adad % 6  == 0 :
+        print(f"{adad}  :  in adad bahale!")
+    elif adad % 9  == 0 :
+        print(f"{adad}  :  in adad bahale!")
+    else :
+        print (adad)
+    adad +=1
